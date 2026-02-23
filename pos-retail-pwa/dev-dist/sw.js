ab7cc80d5cbccfb804b98ebddf05c2ebc32052b2
enHANSced <kennerbarahona.14@hotmail.com>
2026-02-23 09:56:56 -0600

    fix: actualizar la revisión del archivo index.html en el service worker

--- pos-retail-pwa/dev-dist/sw.js
+++ pos-retail-pwa/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5mt6p3v639g"
+    "revision": "0.mshhfhrkt2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
